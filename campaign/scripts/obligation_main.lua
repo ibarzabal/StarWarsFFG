1dca20110019d96078910d51eada06f81d00e1e2
John Ibarzabal <ibarzabal@pobox.com>
2021-04-19 22:36:46 -0300

    Release 20210407

---
+++ campaign/scripts/obligation_main.lua
@@ -0,0 +1,39 @@
+--
+-- Please see the license.html file included with this distribution for
+-- attribution and copyright information.
+--
+
+function onInit()
+	update();
+end
+
+function VisDataCleared()
+	update();
+end
+
+function InvisDataAdded()
+	update();
+end
+
+
+function updateControl(sControl, bReadOnly, bID)
+	if not self[sControl] then
+		return false;
+	end
+
+	if not bID then
+		return self[sControl].update(bReadOnly, true);
+	end
+
+	return self[sControl].update(bReadOnly);
+end
+
+function update()
+	local nodeRecord = getDatabaseNode();
+	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
+	local bID = true; --LibraryData.getIDState("talent", nodeRecord);
+	updateControl("name", bReadOnly, bID);
+	updateControl("value", bReadOnly, bID);
+	updateControl("description", bReadOnly, bID);
+
+end
