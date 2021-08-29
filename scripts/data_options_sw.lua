921e1447d70f7f28a5f15b57c359d75df7dbd7a0
John Ibarzabal <ibarzabal@pobox.com>
2021-08-29 14:43:33 -0300

    Release 20210829

--- scripts/data_options_sw.lua
+++ scripts/data_options_sw.lua
@@ -6,5 +6,7 @@
 function onInit()
 	OptionsManager.addOptionValue("DDCL", "option_val_DDCL_sw_core", "desktopdecal_sw_core", true);
 	OptionsManager.setOptionDefault("DDCL", "desktopdecal_sw_core");
-
+	-- Allow Gear to have attachments
+	OptionsManager.registerOption2("GATT", false, "option_header_sw_house", "option_label_GATT", "option_entry_cycler",
+		{ labels = "option_val_on", values = "on", baselabel = "option_val_off", baseval = "off", default = "on" });
 end
