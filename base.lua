d6383f0a8ac43164143cfff86b103929d25a46ee
John Ibarzabal <ibarzabal@pobox.com>
2021-10-03 20:06:50 -0300

    updating upgrade database code

--- base.lua
+++ base.lua
@@ -3,5 +3,6 @@ function onInit()
 	if User.isHost() then
 		DB.createNode("StoryPointPCchit").setPublic(true);
 		DB.createNode("StoryPointGMchit").setPublic(true);
+		DB.createNode("StarWarsFFG_Campaign").setPublic(false);
 	end
 end
