48e0950aceb36e762d33110dfb3516800da314f4
John Ibarzabal <ibarzabal@pobox.com>
2021-10-07 11:32:39 -0300

    Release 20211007 Fix upgradedb code to properly record database version after upgrade is executed so it will not run everytime the campaign is loaded

--- base.lua
+++ base.lua
@@ -3,6 +3,5 @@ function onInit()
 	if User.isHost() then
 		DB.createNode("StoryPointPCchit").setPublic(true);
 		DB.createNode("StoryPointGMchit").setPublic(true);
-		DB.createNode("StarWarsFFG_Campaign").setPublic(false);
 	end
 end
