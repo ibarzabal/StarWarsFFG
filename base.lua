3856fef44a26d8968ffa6f2345e1e7ea3f8a076b
ibarzabal <ibarzabal@pobox.com>
2022-05-22 02:29:30 -0300

    Release 20220522 - added Party Vehicle Sharing

--- base.lua
+++ base.lua
@@ -3,5 +3,7 @@ function onInit()
 	if User.isHost() then
 		DB.createNode("StoryPointPCchit").setPublic(true);
 		DB.createNode("StoryPointGMchit").setPublic(true);
+		ChatManager.registerSlashCommand("export_nodesc", DBManagerGenesys.BackupDescriptionAndExport);
+		PartyVehicleManager.buildPartySharedVehicles();
 	end
 end
